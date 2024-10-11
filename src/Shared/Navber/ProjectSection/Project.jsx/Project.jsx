d79acb5aed74a9838f0546b796395dd99eab67f2
Moazzem <tiyonbhuiyan.201@gmail.com>
2024-10-12 01:14:06 +0600

    update

--- src/Shared/Navber/ProjectSection/Project.jsx/Project.jsx
+++ src/Shared/Navber/ProjectSection/Project.jsx/Project.jsx
@@ -9,16 +9,22 @@ import img5 from "../../../../assets/p3.png";
 import img6 from "../../../../assets/im6.png";
 import movie from '../../../../assets/movie.png'
 import travel from '../../../../assets/p7.png'
+import car from '../../../../assets/car.png'
 
 import SectionTitle from "../../../SectionTitle/SectionTitle";
 
 // Project data
 const projects = [
-  { id: 1, imgSrc: img4, title: "Ecommerce Gadget website using React for the frontend and Node.js for the backend integrated with MongoDB for data storage. Implemented payment handling using Stripe API.", link: "https://form-auth-with-email--pass.web.app/" },
-  { id: 2, imgSrc: img2, title: "Hungry Horse Full, a complete e-commerce website using React for the frontend and Node.js for the backend integrated with MongoDB for data storage. Implemented payment handling using Stripe API.", link: "https://simple-firebase-fa54a.web.app/" },
-  { id: 3, imgSrc: travel, title: "Travel Booking Website using React js Node js Express js.", link: "https://form-validation-usecntx.web.app/" },
+
+  { id: 1, imgSrc: img2, title: "Hungry Horse Full, a complete e-commerce website using React for the frontend and Node.js for the backend integrated with MongoDB for data storage. Implemented payment handling using Stripe API.", link: "https://simple-firebase-fa54a.web.app/" },
+  { id: 2, imgSrc: travel, title: "Travel Booking Website using React js Node js Express js.  Implemented payment handling using Stripe API.", link: "https://form-validation-usecntx.web.app/" },
+
+  { id: 3, imgSrc: img4, title: "Ecommerce Gadget website using React for the frontend and Node.js for the backend integrated with MongoDB for data storage. ", link: "https://form-auth-with-email--pass.web.app/" },
+
+
+  { id: 4, imgSrc: car, title: " car website  using React.js", link: "https://carwebsite-clientside.vercel.app/" },
   { id: 4, imgSrc: img1, title: "Simple Food Project Frontend using React.js", link: "https://simply-goo-food.web.app/" },
-  { id: 5, imgSrc: img6, title: "New Project - Blog Application using React.js and Tailwind CSS", link: "https://darkmood-and-ligthmodd-toggole-practice.vercel.app/" },
+  // { id: 5, imgSrc: img6, title: "New Project - Blog Application using React.js and Tailwind CSS", link: "https://darkmood-and-ligthmodd-toggole-practice.vercel.app/" },
   { id: 6, imgSrc: movie, title: "Simple Movies-website using react.js and Tailwind CSS", link: "https://search-movies-self.vercel.app/" },
   { id: 7, imgSrc: img5, title: "Restaurant Booking Frontend using React.js", link: "https://resturent-booking-c5002.web.app" },
 ];
@@ -55,11 +61,11 @@ const Project = () => {
                 className="w-full h-60 object-cover rounded-lg shadow-lg group-hover:scale-105 transition-transform duration-500"
               />
               <motion.div
-                className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-500"
+                className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity rounded-xl  duration-500"
                 initial={{ scale: 0.8 }}
                 whileHover={{ scale: 1 }}
               >
-                <h3 className="text-white text-xl font-semibold">
+                <h3 className="text-white text-2xl p-5 ">
                   {project.title}
                 </h3>
               </motion.div>
